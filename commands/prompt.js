f548abacf2e8ddb821fccfd2ee0fcdb53cadd2d7
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-01-26 10:04:24 -0800

    optimize require

--- commands/prompt.js
+++ commands/prompt.js
@@ -2,9 +2,9 @@
 
 const co = require('co')
 const cli = require('heroku-cli-util')
-const inquirer = require('inquirer')
 
 function * run (context) {
+  const inquirer = require('inquirer')
   let answer = yield inquirer.prompt({type: 'password', message: 'enter a password', name: 'password'})
   cli.debug(answer)
 }
